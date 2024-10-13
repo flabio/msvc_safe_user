ca0fcc2ae1f77aead293df34863079b2009d18f6
Flabio Hinestroza Sanclemente <flabiohs@gmail.com>
2024-10-13 16:15:07 -0500

    cnfiguration of cors

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/safe_msvc_user/insfractruture/routers"
 )
 
@@ -9,11 +10,11 @@ func main() {
 	app := fiber.New()
 	// Custom CORS configuration
 	// Enable CORS with specific settings
-	/*app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:3080", // Specify the origin that is allowed to make requests
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "https://34.207.102.227:3080", // Specify the origin that is allowed to make requests
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
-	}))*/
+	}))
 	routers.NewAuthRouter(app)
 	routers.NewUserRouter(app)
 	app.Listen(":3005")
